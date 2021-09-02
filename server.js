f11759c0b039b10904e7337ff00fb929e9e51c89
Upendra12395 <ch95upendra@gmail.com>
2021-09-02 20:24:41 +0530

    main file

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRouter = require("./routes/user");
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+const password = process.env.PASSWORD;
+
+const mongooseURL = `mongodb+srv://upendraa:${password}@cluster0.mwuaz.mongodb.net/letsbuild?retryWrites=true&w=majority`;
+mongoose.connect(mongooseURL, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+});
+
+mongoose.connection.on('open', ()=>{
+    console.log("connected")
+});
+
+app.use(express.json());
+
+
+app.use("/user", userRouter);
+
+app.get('/', (req, res)=>{
+    res.send("hello")
+});
+
+app.listen(PORT, ()=>{
+    console.log(`app running on port ${PORT}`)
+});
