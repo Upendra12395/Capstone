8423aea320965cec6e1c5d6471c840974d441841
Upendra12395 <ch95upendra@gmail.com>
2021-09-16 22:09:11 +0530

    populate a particular field

--- controllers/project.js
+++ controllers/project.js
@@ -28,7 +28,7 @@ module.exports.addProject = async (req, res) => {
   };
 
 module.exports.project = (req, res)=>{
-        Project.find().populate('userId')
+        Project.find().populate('userId', 'userName')
             .select("-password")
             .then((project) => {
                 res.json(project);
