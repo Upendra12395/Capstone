8b248b0e655aab7cbc8d7b4cfdcf88f005fbebd6
Upendra12395 <ch95upendra@gmail.com>
2021-11-05 21:57:14 +0530

    user route and controller for updating profile

--- controllers/user.js
+++ controllers/user.js
@@ -83,3 +83,14 @@ module.exports.getAll = (req, res) => {
 			return res.status(500).json({ message: error.message });
 		});
 };
+
+module.exports.updateProfile = (req, res)=>{
+	const userId = req.user._id
+	Builder.findByIdAndUpdate(userId, req.body, {useModifyAndUpdate : false})
+	.then(user=>{
+		res.status(200).json({message : 'Profile Updated successfully'})
+	})
+	.catch(err =>{
+		res.status(500).json({message : err.message})
+	})
+}
